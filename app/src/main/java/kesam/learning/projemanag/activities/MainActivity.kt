f0ee8e2231dd79fac061f7dbdab1c2dac46777b9
KevSalcedo <kevsalcedom@gmail.com>
2022-11-08 14:04:22 -0500

    Adding The Drawer Functionality In Code

--- app/src/main/java/kesam/learning/projemanag/activities/MainActivity.kt
+++ app/src/main/java/kesam/learning/projemanag/activities/MainActivity.kt
@@ -1,11 +1,22 @@
 package kesam.learning.projemanag.activities
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import kesam.learning.projemanag.R
 import kesam.learning.projemanag.databinding.ActivityMainBinding
 
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener{
 
     private var binding: ActivityMainBinding? = null
 
@@ -13,6 +24,81 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        setupActionBar()
+
+        binding?.navView?.setNavigationItemSelectedListener(this)
+
+
+    }
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding?.appBarMainLayout?.toolbarMainActivity)
+        binding?.appBarMainLayout?.toolbarMainActivity?.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        binding?.appBarMainLayout?.toolbarMainActivity?.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    /**
+     * A function for opening and closing the Navigation Drawer.
+     */
+    private fun toggleDrawer() {
+
+        if (binding?.drawerLayout?.isDrawerOpen(GravityCompat.START) == true){
+            binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        }else{
+            binding?.drawerLayout?.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding?.drawerLayout?.isDrawerOpen(GravityCompat.START) == true){
+            binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        } else {
+            // A double back press function is added in Base Activity.
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        // Add the click events of navigation menu items
+        when (item.itemId) {
+            R.id.nav_my_profile -> {
+
+                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_sign_out -> {
+                // Here sign outs the user from firebase in this device.
+                FirebaseAuth.getInstance().signOut()
+
+                // Send the user to the intro screen of the application.
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        return true
     }
 
 
