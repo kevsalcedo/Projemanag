49292ce31ca6e7159835799974f3a9ada48564f5
KevSalcedo <kevsalcedom@gmail.com>
2022-11-25 15:06:30 -0500

    Image Chooser For The Profile Image

--- app/src/main/java/kesam/learning/projemanag/activities/MyProfileActivity.kt
+++ app/src/main/java/kesam/learning/projemanag/activities/MyProfileActivity.kt
@@ -1,20 +1,32 @@
 package kesam.learning.projemanag.activities
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import kesam.learning.projemanag.R
 import kesam.learning.projemanag.databinding.ActivityMainBinding
 import kesam.learning.projemanag.databinding.ActivityMyProfileBinding
 import kesam.learning.projemanag.firebase.FirestoreClass
 import kesam.learning.projemanag.models.User
+import java.io.IOException
+import java.util.jar.Manifest
 
 class MyProfileActivity : BaseActivity() {
 
     private var binding: ActivityMyProfileBinding? = null
+
+    private var mSelectedImageFileUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +48,85 @@ class MyProfileActivity : BaseActivity() {
 
         // Call a function to get the current logged in user details.
         FirestoreClass().loadUserData(this@MyProfileActivity)
+
+        binding?.ivProfileUserImage?.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }else{
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    // Check the result of runtime permission after the user allows or deny based on the unique code.
+    /**
+     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            // Call the image chooser function.
+            showImageChooser()
+        }else{
+            //Displaying another toast if permission is not granted
+            Toast.makeText(
+                this,
+                "Oops, you just denied the permission for storage. You can also allow it from settings.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    // Get the result of the image selection based on the constant code.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+            // The uri of selection image from phone storage.
+            mSelectedImageFileUri = data.data
+
+            try {
+                // Load the user image in the ImageView.
+                binding?.ivProfileUserImage?.let {
+                    Glide
+                        .with(this@MyProfileActivity)
+                        .load(Uri.parse(mSelectedImageFileUri.toString())) // URI of the image
+                        .centerCrop() // Scale type of the image.
+                        .placeholder(R.drawable.ic_user_place_holder) // A default place holder
+                        .into(it) // the view in which the image will be loaded. (Context: it = ivProfileUserImage)
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    /**
+     * A function for user profile image selection from phone storage.
+     */
+    private fun showImageChooser() {
+        // An intent for launching the image selection of phone storage.
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        // Launches the image selection of phone storage using the constant code.
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
     }
 
     private fun setupActionBar() {
@@ -56,7 +147,7 @@ class MyProfileActivity : BaseActivity() {
      * A function to set the existing details in UI.
      */
     fun setUserDataInUI(user: User) {
-        binding?.ivUserImage?.let {
+        binding?.ivProfileUserImage?.let {
             Glide
                 .with(this@MyProfileActivity)
                 .load(user.image)
@@ -70,6 +161,17 @@ class MyProfileActivity : BaseActivity() {
         if (user.mobile != 0L) {
             binding?.etMobile?.setText(user.mobile.toString())
         }
+    }
+
+    /**
+     * A companion object to declare the constants.
+     */
+    companion object {
+        //A unique code for asking the Read Storage Permission using this we will be check and identify in the method onRequestPermissionsResult
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+
+        // Add a constant for image selection from phone storage
+        private const val PICK_IMAGE_REQUEST_CODE = 2
     }
 
 
